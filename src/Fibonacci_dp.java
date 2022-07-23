09b6625466f72e0b21af9fc1320642bfaad44967
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-23 18:23:01 +0530

    Create a int type variable named as fibonacci_no which stores the final fibonacci value with the call of Fibonacci method from main method.

--- src/Fibonacci_dp.java
+++ src/Fibonacci_dp.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 public class Fibonacci_dp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int fibonacci_no = Fibonacci(number, new int[number+1]);
 
     }
     public static int Fibonacci(int n, int[] dp_array){
