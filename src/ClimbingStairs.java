a82d71fa5047f91931a024362568208fc87cdca9
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-24 15:12:18 +0530

    Import the Scanner class to take input

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,15 @@
+/*
+ * Author Name : Vishad Raj Vashishtha
+ * IDE: IntelliJ IDEA Community Edition
+ * Date: 24-07-2022
+ */
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+    }
+}
