9b3bc76ea2c0f76e35255bc678a11af40b90f7a8
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-24 15:16:47 +0530

    Create a method named as countPath which takes two parameter and return an integer value.

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -10,6 +10,11 @@ public class ClimbingStairs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        int totalPathCount = countPath(n, new int[n+1]);
+        System.out.println(totalPathCount);
+    }
 
+    public static int countPath(int n, int [] array){
+        return 0;
     }
 }
