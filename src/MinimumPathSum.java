d3f4176d62aade74ea28a5098c6398b45b9a438b
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-26 00:35:27 +0530

    Create a 2D array or matrix named as array

--- src/MinimumPathSum.java
+++ src/MinimumPathSum.java
@@ -4,8 +4,16 @@
  * Date: 25-07-2022
  */
 
+import java.util.Scanner;
+
 public class MinimumPathSum {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int col = input.nextInt();
+
+        int [][] array = new int[row][col];
+
 
     }
 }
