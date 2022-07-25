b198d0b5e6c30818b6af433718c6275f73926cc0
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-25 22:47:24 +0530

    Create a MinimumPathSum Class

--- src/ClimbingStairsWIthMinimumMoves.java
+++ src/ClimbingStairsWIthMinimumMoves.java
@@ -11,17 +11,21 @@ public class ClimbingStairsWIthMinimumMoves {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++)
+        {
             arr[i]=input.nextInt();
         }
         // Creat a DP array of Size n+1
         Integer [] dp = new Integer[n+1];
-        dp[n]=0;
-        for(int i=n-1; i>=0; i--){
+        dp[n]=0; // we are declaring the n index of dp array to Zero
+        for(int i=n-1; i>=0; i--)
+        {
             if(arr[i]>0) {
-                int min = Integer.MAX_VALUE;
-                for (int j = 1; j <= arr[i] && i+j < dp.length; j++) {
-                    if(dp[i+j] != null){
+                int min = Integer.MAX_VALUE; // declaring the min value
+                for (int j = 1; j <= arr[i] && i+j < dp.length; j++)
+                {
+                    if(dp[i+j] != null)
+                    {
                         min = Math.min(min, dp[i+j]);
                     }
                 }
---
+++ src/MinimumPathSum.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name : Vishad Raj Vashishtha
+ * IDE: IntelliJ IDEA Community Edition
+ * Date: 25-07-2022
+ */
+
+public class MinimumPathSum {
+    public static void main(String[] args) {
+
+    }
+}
