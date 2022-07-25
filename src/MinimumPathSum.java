b20603fc88fdb6d6e6c00fc420dd57462522fc13
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-26 00:37:11 +0530

    initializing the 2D Matrix using for loop

--- src/MinimumPathSum.java
+++ src/MinimumPathSum.java
@@ -13,6 +13,11 @@ public class MinimumPathSum {
         int col = input.nextInt();
 
         int [][] array = new int[row][col];
+        for (int i=0;i<array.length; i++){
+            for(int j=0; j<array[0].length; j++) {
+                array[i][j] = input.nextInt();
+            }
+        }
 
 
     }
