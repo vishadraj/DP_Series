1ca747482c06850add2406570d7454b1e4fc47bb
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-07-26 23:34:23 +0530

    Take row and column as input and declare the array of size row and column

--- src/PathWithMinimumGold.java
+++ src/PathWithMinimumGold.java
@@ -4,8 +4,14 @@
  * Date: 26-07-2022
  */
 
+import java.util.Scanner;
+
 public class PathWithMinimumGold {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int col = input.nextInt();
+        int [][] array = new int[row][col];
+        
     }
 }
